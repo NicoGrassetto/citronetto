47d7880d84fd607327c9b207cd381d5039b1480d
Nico Grassetto <nico.grassetto@agilytic.be>
2023-09-02 22:48:30 +0200

    Added documentation

--- database.js
+++ database.js
@@ -1,6 +1,17 @@
 const Sequelize = require('sequelize');
 const { TODOList } = require('./templates/listTemplate.js');
+
+/**
+ * Represents a database for managing TODO lists.
+ *
+ * @class Database
+ */
 class Database {
+	/**
+	 * Creates a new Database instance.
+	 *
+	 * @constructor
+	 */
 	constructor() {
 		this.sequelize = new Sequelize('database', 'user', 'password', {
 			host: 'localhost',
@@ -20,10 +31,25 @@ class Database {
 		});
 	}
 
+	/**
+	 * Synchronizes the database schema.
+	 *
+	 * @memberof Database
+	 * @function sync
+	 */
 	sync() {
 		this.Lists.sync();
 	}
 
+	/**
+	 * Adds a new TODO list to the database.
+	 *
+	 * @param {string} tag - The tag associated with the TODO list.
+	 * @param {string} todoList - A string representation of a TODO list.
+	 * @throws {Error} If tag is not a string or todoList is not a string representation of TODOList.
+	 * @memberof Database
+	 * @function addTODOList
+	 */
 	async addTODOList(tag, todoList) {
 		if (typeof tag !== 'string') {
 			throw new Error('tag should be a string.');
@@ -41,7 +67,15 @@ class Database {
 			list: todoList,
 		});
 	}
-
+	/**
+	 * Checks if a TODO list with the given tag already exists in the database.
+	 *
+	 * @param {string} tag - The tag to check for existence.
+	 * @returns {boolean} True if a TODO list with the given tag exists, otherwise false.
+	 * @throws {Error} If tag is not a string.
+	 * @memberof Database
+	 * @function alreadyInDB
+	 */
 	async alreadyInDB(tag) {
 		if (typeof tag !== 'string') {
 			throw new Error('tag should be a string.');
@@ -56,6 +90,16 @@ class Database {
 		}
 	}
 
+	/**
+	 * Deletes a task from a TODO list with the given tag at the specified index.
+	 *
+	 * @param {string} tag - The tag associated with the TODO list.
+	 * @param {number} index - The index of the task to delete.
+	 * @returns {Object} The updated TODO list after task deletion.
+	 * @throws {Error} If tag is not a string.
+	 * @memberof Database
+	 * @function deleteTaskFromTODOList
+	 */
 	async deleteTaskFromTODOList(tag, index) {
 		if (typeof tag !== 'string') {
 			throw new Error('tag should be a string.');
@@ -77,6 +121,15 @@ class Database {
 		}
 	}
 
+	/**
+	 * Resets a TODO list with the given tag, effectively deleting it from the database.
+	 *
+	 * @param {string} tag - The tag associated with the TODO list to reset.
+	 * @returns {Promise} A Promise that resolves when the TODO list is successfully reset.
+	 * @throws {Error} If tag is not a string.
+	 * @memberof Database
+	 * @function resetTODOList
+	 */
 	async resetTODOList(tag) {
 		if (typeof tag !== 'string') {
 			throw new Error('tag should be a string.');
@@ -84,6 +137,16 @@ class Database {
 		return await this.Lists.destroy({ where: { tag: tag } });
 	}
 
+	/**
+	 * Undoes the status of a task in a TODO list with the given tag at the specified index.
+	 *
+	 * @param {string} tag - The tag associated with the TODO list.
+	 * @param {number} index - The index of the task to undo.
+	 * @returns {Object} The updated TODO list after task status update.
+	 * @throws {Error} If tag is not a string.
+	 * @memberof Database
+	 * @function undoTask
+	 */
 	async undoTask(tag, index) {
 		if (typeof tag !== 'string') {
 			throw new Error('tag should be a string.');
@@ -98,6 +161,16 @@ class Database {
 		return JSON.parse(listStr);
 	}
 
+	/**
+	 * Modifies the description of a task in a TODO list with the given tag at the specified index.
+	 *
+	 * @param {string} tag - The tag associated with the TODO list.
+	 * @param {number} index - The index of the task to modify.
+	 * @param {string} newDescription - The new description for the task.
+	 * @returns {Object} The updated TODO list after task description modification.
+	 * @memberof Database
+	 * @function modifyTaskDescriptionFromTODOList
+	 */
 	async modifyTaskDescriptionFromTODOList(tag, index, newDescription) {
 		const userList = await this.Lists.findOne({ where: { tag: tag } });
 		const listData = JSON.parse(userList['dataValues']['list']);
@@ -108,6 +181,16 @@ class Database {
 		return JSON.parse(listStr);
 	}
 
+	/**
+	 * Sets the status of a task in a TODO list with the given tag at the specified index to "done."
+	 *
+	 * @param {string} tag - The tag associated with the TODO list.
+	 * @param {number} index - The index of the task to mark as "done."
+	 * @returns {Object} The updated TODO list after task status update.
+	 * @throws {Error} If tag is not a string.
+	 * @memberof Database
+	 * @function setTaskToDone
+	 */
 	async setTaskToDone(tag, index) {
 		if (typeof tag !== 'string') {
 			throw new Error('tag should be a string.');
@@ -122,6 +205,15 @@ class Database {
 		return JSON.parse(listStr);
 	}
 
+	/**
+	 * Adds a new task to a TODO list with the given tag, appending it to the end of the list.
+	 *
+	 * @param {string} tag - The tag associated with the TODO list.
+	 * @param {string} description - The description of the new task.
+	 * @returns {Object} The updated TODO list after task addition.
+	 * @memberof Database
+	 * @function addTaskToTODOList
+	 */
 	async addTaskToTODOList(tag, description) {
 		const userList = await this.Lists.findOne({ where: { tag: tag } });
 		const listData = JSON.parse(userList['dataValues']['list']);
@@ -132,6 +224,15 @@ class Database {
 		return JSON.parse(listStr);
 	}
 
+	/**
+	 * Retrieves the TODO list associated with the given tag from the database.
+	 *
+	 * @param {string} tag - The tag associated with the TODO list to retrieve.
+	 * @returns {Object} The retrieved TODO list.
+	 * @throws {Error} If tag is not a string.
+	 * @memberof Database
+	 * @function getTODOList
+	 */
 	async getTODOList(tag) {
 		if (typeof tag !== 'string') {
 			throw new Error('tag should be a string.');
--- templates/listTemplate.js
+++ templates/listTemplate.js
@@ -1,4 +1,17 @@
+/**
+ * Represents a TODO List with tasks and their statuses.
+ *
+ * @class TODOList
+ */
 class TODOList {
+	/**
+	 * Creates a new TODOList instance.
+	 *
+	 * @constructor
+	 * @param {string[]} descriptions - An array of task descriptions.
+	 * @param {boolean[]} [statuses] - An optional array of task statuses.
+	 * @throws {Error} If descriptions is not an array or is empty, or if arguments are invalid.
+	 */
 	constructor(descriptions, statuses) {
 		if (!Array.isArray(descriptions) || descriptions.length === 0) {
 			throw new Error('descriptions must contain at least one task.');
@@ -18,7 +31,12 @@ class TODOList {
 			throw new Error('Argument error: must contain at least a descriptions array.');
 		}
 	}
-
+	/**
+	 * Adds a new task to the TODO list.
+	 *
+	 * @param {string} taskDescription - The description of the task to add.
+	 * @throws {Error} If taskDescription is not a string.
+	 */
 	addTask(taskDescription) {
 		if (typeof taskDescription !== 'string') {
 			throw new Error('taskDescription must be a string.');
@@ -28,6 +46,12 @@ class TODOList {
 		this.statuses.push(false);
 	}
 
+	/**
+	 * Deletes a task from the TODO list.
+	 *
+	 * @param {number} index - The index of the task to delete.
+	 * @throws {Error} If index is out of bounds.
+	 */
 	deleteTask(index) {
 		if (index >= 0 && index < this.descriptions.length) {
 			this.descriptions.splice(index, 1);
@@ -38,6 +62,13 @@ class TODOList {
 		}
 	}
 
+	/**
+	 * Sets the description of a task.
+	 *
+	 * @param {string} description - The new description for the task.
+	 * @param {number} index - The index of the task to update.
+	 * @throws {Error} If description is not a string or if index is out of bounds.
+	 */
 	setDescription(description, index) {
 		if (typeof description !== 'string') {
 			throw new Error('description must be a string.');
@@ -50,6 +81,13 @@ class TODOList {
 		}
 	}
 
+	/**
+	 * Sets the status of a task.
+	 *
+	 * @param {boolean} status - The new status for the task.
+	 * @param {number} index - The index of the task to update.
+	 * @throws {Error} If status is not a boolean or if index is out of bounds.
+	 */
 	setStatus(status, index) {
 		if (typeof status !== 'boolean') {
 			throw new Error('status must be a boolean.');
